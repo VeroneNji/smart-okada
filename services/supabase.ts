51d1ed2a8a1499d5c71db03be7761b99ecf8034e
Verone NJI <veronenji2023@gmail.com>
2026-05-01 12:50:15 +0100

    fix supabase connection

--- services/supabase.ts
+++ services/supabase.ts
@@ -1,11 +1,12 @@
-import 'react-native-url-polyfill/auto';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js';
-import { Database } from '../types/database.types';
 import { Platform } from 'react-native';
+import 'react-native-url-polyfill/auto';
+import { Database } from '../types/database.types';
 
-const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL || '';
-const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY || '';
+const supabaseUrl = 'https://hovavzvyuyodknfgdmmt.supabase.co';
+const supabaseAnonKey = 'sb_publishable_DG2S_cegmPJGY8qtwm2Aag_0r2so4-W';
+
 
 // Custom storage wrapper to handle SSR (Server Side Rendering) environments
 const ExpoSqliteStorage = {
